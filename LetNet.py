01d62c19aa51cf3ca2f5f97e336b81fab62e97cb
BruceScu <bruce_scu@163.com>
2023-11-06 18:34:41 +0800

    #

--- LetNet.py
+++ LetNet.py
@@ -48,9 +48,14 @@ def training(net, train_iter, test_iter, num_epochs, lr, device):
     net.to(device)
     optimizer = torch.optim.SGD(net.parameters(), lr=lr)
     loss = nn.CrossEntropyLoss()
+    animator = d2l.Animator(xlabel='epoch', xlim=[1,num_epochs],
+                            legend=['train loss', 'train acc', 'test acc'])
+    timer, num_batches = d2l.Timer(), len(train_iter)
 
     #timer, num_batches = d2l.Timer(), len(train_iter)
     for epoch in range(num_epochs):
+        # 训练损失之和，训练准确率之和，样本数
+        metric = d2l.Accumulator(3)
         net.train()
         for i, (X, y) in enumerate(train_iter):
             optimizer.zero_grad()
@@ -59,7 +64,21 @@ def training(net, train_iter, test_iter, num_epochs, lr, device):
             l = loss(y_hat, y)
             l.backward()
             optimizer.step()
+            with torch.no_grad():
+                metric.add(l*X.shape[0], d2l.accuracy(y_hat, y), X.shape[0])
+            timer.stop()
+            train_l = metric[0] / metric[2]
+            train_acc = metric[1] / metric[2]
+            if (i+1) % (num_batches//5) == 0 or i == num_batches - 1:
+                animator.add(epoch+(i+1)/num_batches,
+                             (train_l, train_acc, None))
+        test_acc = evaluate_acc_gpu(net, test_iter)
+        animator.add(epoch+1,(None,None,test_acc))
+    print(f'loss {train_l:.3f}, train acc {train_acc:.3f}, '
+          f'test acc {test_acc:.3f}')
+    print(f'{metric[2] * num_epochs / timer.sum():.1f} examples/sec '
+          f'on {str(device)}')
 
 training(net=net, train_iter=train_iter, test_iter=test_iter,
-         num_epochs=10, lr=0.9, device=torch.device('mps:0'))
-
+         num_epochs=10, lr=0.9, device=d2l.try_gpu())
+d2l.plt.show()
